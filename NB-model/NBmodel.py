366f7e24e4a386f4ebc3ccdf1e88792a93ce7865
girishpillai17 <girishpillai239@gmail.com>
2020-10-01 23:26:38 +0530

    NB Model file uploaded

---
+++ NB-model/NBmodel.py
@@ -0,0 +1,17 @@
+import numpy as np
+import logging
+import pickle
+import time
+import sklearn
+
+__version__ = "0.1"
+logger = logging.gerLogger(__name__)
+
+class NBmodel(object):
+    def __init__(self):
+        logger.info('Starting %s Microservice, version %s', __name__, __version__)
+        self.model = pickle.load(open("NBmodel.pkl", "rb"))
+
+    def predict(self, X, features_names=None):
+        print(X)
+        return self.model.predict(X)
