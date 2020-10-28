c684dbd6eb8baf8550bacbe6784fc46f526aa1b0
girishpillai17 <36442095+girishpillai17@users.noreply.github.com>
2020-10-28 17:48:04 +0530

    Update NBmodel.py

--- NB-model/NBmodel.py
+++ NB-model/NBmodel.py
@@ -4,12 +4,8 @@ import pickle
 import time
 import sklearn
 
-__version__ = "0.1"
-logger = logging.gerLogger(__name__)
-
 class NBmodel(object):
     def __init__(self):
-        logger.info('Starting %s Microservice, version %s', __name__, __version__)
         self.model = pickle.load(open("NBmodel.pkl", "rb"))
 
     def predict(self, X, features_names=None):
