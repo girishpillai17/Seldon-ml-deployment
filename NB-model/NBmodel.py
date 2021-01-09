5e732c91c6e0081b67fdc197929e730ec5682a8f
Girish Pillai <36442095+girishpillai17@users.noreply.github.com>
2021-01-09 22:44:11 +0530

    Update NBmodel.py

--- NB-model/NBmodel.py
+++ NB-model/NBmodel.py
@@ -7,7 +7,8 @@ import sklearn
 class NBModel(object):
     def __init__(self):
         self.model = pickle.load(open("NBmodel.pkl", "rb"))
-
+    
+    # wrap your machine learning model create a Class that has a predict method
     def predict(self, X, features_names=None):
         print(X)
         return self.model.predict([X])
